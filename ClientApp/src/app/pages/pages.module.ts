d7e73c9f8a0b7a8dbacd0bf761fafa966834fca6
sorosh.saloti <sorosh.saloti@pishtazan.onmicrosoft.com>
2022-03-13 08:25:21 +0300

    add in git

---
+++ ClientApp/src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+
+const PAGES_COMPONENTS = [
+  PagesComponent,
+  FetchDataComponent,
+];
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    ECommerceModule,
+    MiscellaneousModule,
+  ],
+  declarations: [
+    ...PAGES_COMPONENTS,
+  ],
+})
+export class PagesModule {
+}
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# asp.net-core-ngx-admin-master
-This is ngx-admin config for .net core this project created because ngx admin in 2019 just config for angualr and your are not able use in .net core angular.
+# ASP.NET Core ngx-admin Integration
+
+- ngx-admin 2.3
+- dotnet core 2.1.402
+
+## Integration Steps.
+1. Install .Net Core SDK.
+2. create project folder, cd into, run `dotnet new angular` in cmd/terminal.
+3. Delete everything inside 'ClientApp' folder.
+4. cd into ClientApp, `git clone` ngx-admin.
+5. cd to the project folder, run `dotnet run`.
+6. http://localhost:5000/
+
+## Had reverted back the `fetch-data.component` inside of `ClientApp/src/app/pages` folder.
+https://localhost:5000/#/pages/fetch-data
 
